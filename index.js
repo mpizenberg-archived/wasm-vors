469d0c702d23ae372ba975903bb093e33801578d
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-08-22 14:12:01 +0200

    Re-arrange JS file to free file reader memory at some point

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 import { WasmTracker, default as init } from "./pkg/wasm_vors.js";
 
 // Globals.
-let stats = new Stats();
+const stats = new Stats();
+const file_input = document.getElementById("file-input");
+
+// Prepare DOM.
 document.body.appendChild(stats.dom);
 
-const file_input = document.getElementById("file-input");
-const file_reader = new FileReader();
-file_input.addEventListener("change", () => loadInput(file_input));
+// Run Forest!
+run();
 
 async function run() {
 	// Initialize the wasm module.
 	const wasm = await init("./pkg/wasm_vors_bg.wasm");
 	const wasm_tracker = WasmTracker.new();
 
-	// Transfer archive data to wasm when the file is loaded.
-	file_reader.onload = () =>
-		transferContent(file_reader.result, wasm_tracker, wasm);
-}
+	// Set up file reader.
+	let file_reader = new FileReader();
+	file_input.addEventListener("change", () => {
+		const tar_file = file_input.files[0];
+		console.log("Loading tar file ...");
+		file_reader.readAsArrayBuffer(tar_file);
+	});
 
-function loadInput(input) {
-	const tar_file = input.files[0];
-	file_reader.readAsArrayBuffer(tar_file);
+	// Transfer archive data to wasm when the file is loaded.
+	file_reader.onload = () => {
+		console.log("Transfering tar data to wasm memory ...");
+		transferContent(file_reader.result, wasm_tracker, wasm);
+		console.log("Initializing tracker with first image ...");
+		const nb_frames = wasm_tracker.init("icl");
+		console.log("Starting animation frame loop ...");
+		window.requestAnimationFrame(() => track(wasm_tracker, 1, nb_frames));
+		file_reader = null; // Free memory.
+	};
 }
 
 // Transfer archive data to wasm when the file is loaded.
@@ -28,14 +40,11 @@ function transferContent(arrayBuffer, wasm_tracker, wasm) {
 	wasm_tracker.allocate(arrayBuffer.byteLength);
 	const wasm_buffer = new Uint8Array(wasm.memory.buffer);
 	const start = wasm_tracker.memory_pos();
-	const file_buffer = new Uint8Array(arrayBuffer);
+	let file_buffer = new Uint8Array(arrayBuffer);
 	wasm_buffer.set(file_buffer, start);
+	file_buffer = null; arrayBuffer = null; // Free memory.
 	console.log("Building entries hash map ...");
 	wasm_tracker.build_entries_map();
-	console.log("Initializing tracker with first image ...");
-	const nb_frames = wasm_tracker.init("fr3");
-	console.log("Starting animation frame loop ...");
-	window.requestAnimationFrame(() => track(wasm_tracker, 1, nb_frames));
 }
 
 function track(wasm_tracker, frame_id, nb_frames) {
@@ -48,5 +57,3 @@ function track(wasm_tracker, frame_id, nb_frames) {
 		);
 	}
 }
-
-run();
